1d467713f58f2110f375237e9ce5acfd252314cb
CaparicaLeo <57023740005@unicentro.edu.br>
2025-07-29 22:28:04 -0300

    changing timeout

--- scrapping.js
+++ scrapping.js
@@ -170,7 +170,7 @@ async function filtrarEExtrairLista(page, json) {
 		await page.click(`.abrir-cidades-${json.ESTADO}`);
 		await new Promise((resolve) => setTimeout(resolve, 30000));
 		await page.type(".campo-localidade", json.CIDADE);
-		await new Promise((resolve) => setTimeout(resolve, 30000));
+		await page.waitForSelector(`#retorno-busca-localidade-pj-${json.ESTADO} > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)`,{ visible: true, timeout: 90000 })
 
 		await page.click(
 			`#retorno-busca-localidade-pj-${json.ESTADO} > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)`
